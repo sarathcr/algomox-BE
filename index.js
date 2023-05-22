2737e1416d4dcd64a9ef504b1b9e8645c387cd4f
sarathcr <sarathcr9177725@gmail.com>
2023-05-22 13:42:32 +0530

    Formatted code

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
-const express = require('express')
+const express = require("express");
 
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const routes = require('./routes/routes')
+const routes = require("./routes/routes");
 
-const cors = require('cors')
+const cors = require("cors");
 
+const cookieParser = require("cookie-parser");
 
+const app = express();
 
-
-const cookieParser = require('cookie-parser')
-
-const app = express()
-
-app.use(cors({
+app.use(
+  cors({
     credentials: true,
-    origin:'http://localhost:4200'
-}))
+    origin: "http://localhost:4200",
+  })
+);
 
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use(express.json())
+app.use(express.json());
 
-app.use("/api",routes)
+app.use("/api", routes);
 
-mongoose.connect("mongodb://localhost:27017/algomox",
-{useNewUrlParser: true}).then(() => {
-    console.log("connected to DB")
-    app.listen(5000,() => {
-        console.log("App is running on port 5000")
-    }) 
-})
+mongoose.connect("mongodb://localhost:27017/algomox", { useNewUrlParser: true }).then(() => {
+  console.log("connected to DB");
+  app.listen(5000, () => {
+    console.log("App is running on port 5000");
+  });
+});
--- routes/routes.js
+++ routes/routes.js
@@ -1,231 +1,222 @@
-const {Router} = require('express')
+const { Router } = require("express");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const bcrypt = require('bcryptjs')
+const bcrypt = require("bcryptjs");
 
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-const User = require("../models/user")
+const User = require("../models/user");
 
-const Tasks = require("../models/tasks")
+const Tasks = require("../models/tasks");
 
-const router = Router()
+const router = Router();
 
-router.post('/register', async (req, res)=>{
-    let email = req.body.email
-    let password = req.body.password
-    let name = req.body.name
+router.post("/register", async (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  let name = req.body.name;
 
-    const salt = await bcrypt.genSalt(10)
+  const salt = await bcrypt.genSalt(10);
 
-    const hashedPassword = await bcrypt.hash(password,salt)
+  const hashedPassword = await bcrypt.hash(password, salt);
 
-    const record = await User.findOne({email: email})
+  const record = await User.findOne({ email: email });
 
-    if(record) {
-         return res.status(400).send({
-                message: "Email is already registered",
-         })
-    } else  {
-        const user = new User({
-            name: name,
-            email: email,
-            password: hashedPassword
-        })
-        
-        const result = await user.save()
+  if (record) {
+    return res.status(400).send({
+      message: "Email is already registered",
+    });
+  } else {
+    const user = new User({
+      name: name,
+      email: email,
+      password: hashedPassword,
+    });
 
-        // JWT Token
+    const result = await user.save();
 
-        const {_id} = await result.toJSON()
+    // JWT Token
 
-        const token = jwt.sign({_id:_id}, "secret")
+    const { _id } = await result.toJSON();
 
-        res.cookie("jwt",token,{
-            httpOnly: true,
-            maxAge: 24*60*60*1000
-        })
+    const token = jwt.sign({ _id: _id }, "secret");
 
-        res.send({
-            message: "success"
-        })
-     }
-
-})
-
-router.post("/login", async (req, res) => {
-    const user = await User.findOne({ email: req.body.email });
-  
-    if (!user) {
-      return res.status(404).send({
-        message: "User not Found",
-      });
-    }
-  
-    if (!(await bcrypt.compare(req.body.password, user.password))) {
-      return res.status(400).send({
-        message: "Password is Incorrect",
-      });
-    }
-  
-    const token = jwt.sign({ _id: user._id }, "secret");
-  
     res.cookie("jwt", token, {
       httpOnly: true,
-      maxAge: 24 * 60 * 60 * 1000, // 1 day
+      maxAge: 24 * 60 * 60 * 1000,
     });
-  
+
     res.send({
       message: "success",
     });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return res.status(404).send({
+      message: "User not Found",
+    });
+  }
+
+  if (!(await bcrypt.compare(req.body.password, user.password))) {
+    return res.status(400).send({
+      message: "Password is Incorrect",
+    });
+  }
+
+  const token = jwt.sign({ _id: user._id }, "secret");
+
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000, // 1 day
   });
 
+  res.send({
+    message: "success",
+  });
+});
+
 router.get("/user", async (req, res) => {
-    try {
-      const cookie = req.cookies["jwt"];
-   
-      const claims = jwt.verify(cookie, "secret");
-   
-      if (!claims) {
-        return res.status(401).send({
-          message: "unauthenticated",
-        });
-      }
-   
-      const user = await User.findOne({ _id: claims._id });
-   
-      const { password, ...data } = await user.toJSON();
-   
-      res.send(data);
-    } catch (e) {
+  try {
+    const cookie = req.cookies["jwt"];
+
+    const claims = jwt.verify(cookie, "secret");
+
+    if (!claims) {
       return res.status(401).send({
         message: "unauthenticated",
       });
     }
-  });
 
-  router.post("/logout", (req, res) => {
-    res.cookie("jwt", "", { maxAge: 0 });
-   
-    res.send({
-      message: "success",
+    const user = await User.findOne({ _id: claims._id });
+
+    const { password, ...data } = await user.toJSON();
+
+    res.send(data);
+  } catch (e) {
+    return res.status(401).send({
+      message: "unauthenticated",
     });
-  });
+  }
+});
 
-  // Task GET
-  router.get('/task', async (req, res) => {
-    try {
-      const tasks = await Tasks.find().exec();
-      res.send(tasks);
-    } catch (err) {
-      console.log("Error occurred", err);
-      res.status(400).send({
-        message: "Internal Error Occurred"
-      });
-    }
+router.post("/logout", (req, res) => {
+  res.cookie("jwt", "", { maxAge: 0 });
+
+  res.send({
+    message: "success",
   });
-  
-  //Get by Id
-  router.get('/task/:id', async (req, res) => {
-    try {
-      if (mongoose.Types.ObjectId.isValid(req.params.id)) {
-        const task = await Tasks.findById(req.params.id).exec();
-        if (task) {
-          res.send(task);
-        } else {
-          res.status(404).send("No record found for ID: " + req.params.id);
-        }
+});
+
+// Task GET
+router.get("/task", async (req, res) => {
+  try {
+    const tasks = await Tasks.find().exec();
+    res.send(tasks);
+  } catch (err) {
+    console.log("Error occurred", err);
+    res.status(400).send({
+      message: "Internal Error Occurred",
+    });
+  }
+});
+
+//Get by Id
+router.get("/task/:id", async (req, res) => {
+  try {
+    if (mongoose.Types.ObjectId.isValid(req.params.id)) {
+      const task = await Tasks.findById(req.params.id).exec();
+      if (task) {
+        res.send(task);
       } else {
-        res.status(400).send("Invalid ID format: " + req.params.id);
+        res.status(404).send("No record found for ID: " + req.params.id);
       }
-    } catch (err) {
-      console.log("Internal Error", err);
-      res.status(500).send("Internal Error", err);
+    } else {
+      res.status(400).send("Invalid ID format: " + req.params.id);
     }
-  });
+  } catch (err) {
+    console.log("Internal Error", err);
+    res.status(500).send("Internal Error", err);
+  }
+});
 
-  // Task POST
-  router.post('/task', async (req, res) => {
-    try {
-      let task = new Tasks({
-        title: req.body.title,
-        description: req.body.description,
-        dueDate: req.body.dueDate
-      });
-      const doc = await task.save();
-      res.send(doc);
-    } catch (err) {
-      console.log("Internal Error", err);
-      res.status(400).send("Internal Error", err);
-    }
-  });
-  
+// Task POST
+router.post("/task", async (req, res) => {
+  try {
+    let task = new Tasks({
+      title: req.body.title,
+      description: req.body.description,
+      dueDate: req.body.dueDate,
+    });
+    const doc = await task.save();
+    res.send(doc);
+  } catch (err) {
+    console.log("Internal Error", err);
+    res.status(400).send("Internal Error", err);
+  }
+});
 
-  // DELETE TASKS
-  router.delete('/task/:id', async (req, res) => {
-    try {
-      if (mongoose.Types.ObjectId.isValid(req.params.id)) {
-        const removedTask = await Tasks.findByIdAndRemove(req.params.id).exec();
-        if (removedTask) {
-          res.send(removedTask);
-        } else {
-          res.status(404).send("No record found for ID: " + req.params.id);
-        }
+// DELETE TASKS
+router.delete("/task/:id", async (req, res) => {
+  try {
+    if (mongoose.Types.ObjectId.isValid(req.params.id)) {
+      const removedTask = await Tasks.findByIdAndRemove(req.params.id).exec();
+      if (removedTask) {
+        res.send(removedTask);
       } else {
-        res.status(400).send("Invalid ID format: " + req.params.id);
+        res.status(404).send("No record found for ID: " + req.params.id);
       }
-    } catch (err) {
-      console.log("Internal Error", err);
-      res.status(500).send("Internal Error", err);
+    } else {
+      res.status(400).send("Invalid ID format: " + req.params.id);
     }
-  });
+  } catch (err) {
+    console.log("Internal Error", err);
+    res.status(500).send("Internal Error", err);
+  }
+});
 
+// search
+router.get("/task/search/:query", async (req, res) => {
+  const { query } = req.params;
 
-  // search
-  router.get('/task/search/:query', async (req, res) => {
-    const { query } = req.params;
-  
-    try {
-      const tasks = await Tasks.find({
-        $or: [
-          { title: { $regex: query, $options: 'i' } },
-          { description: { $regex: query, $options: 'i' } },
-        ],
-      }).exec();
-  
-      res.send(tasks);
-    } catch (err) {
-      console.log('Internal Error', err);
-      res.status(500).send('Internal Error');
-    }
-  });
-  
-  
-  
+  try {
+    const tasks = await Tasks.find({
+      $or: [{ title: { $regex: query, $options: "i" } }, { description: { $regex: query, $options: "i" } }],
+    }).exec();
 
-  // Update By id
-  router.put('/task/:id', async (req, res) => {
-    try {
-      let task = {
-        title: req.body.title,
-        description: req.body.description,
-        dueDate: req.body.dueDate
-      };
-      if (mongoose.Types.ObjectId.isValid(req.params.id)) {
-        const updatedTask = await Tasks.findByIdAndUpdate(req.params.id, task, { new: true }).exec();
-        if (updatedTask) {
-          res.send(updatedTask);
-        } else {
-          res.status(404).send("No record found for ID: " + req.params.id);
-        }
+    res.send(tasks);
+  } catch (err) {
+    console.log("Internal Error", err);
+    res.status(500).send("Internal Error");
+  }
+});
+
+// Update By id
+router.put("/task/:id", async (req, res) => {
+  try {
+    let task = {
+      title: req.body.title,
+      description: req.body.description,
+      dueDate: req.body.dueDate,
+    };
+    if (mongoose.Types.ObjectId.isValid(req.params.id)) {
+      const updatedTask = await Tasks.findByIdAndUpdate(req.params.id, task, { new: true }).exec();
+      if (updatedTask) {
+        res.send(updatedTask);
       } else {
-        res.status(400).send("Invalid ID format: " + req.params.id);
+        res.status(404).send("No record found for ID: " + req.params.id);
       }
-    } catch (err) {
-      console.log("Internal Error", err);
-      res.status(500).send("Internal Error", err);
+    } else {
+      res.status(400).send("Invalid ID format: " + req.params.id);
     }
-  });
+  } catch (err) {
+    console.log("Internal Error", err);
+    res.status(500).send("Internal Error", err);
+  }
+});
 
-module.exports = router
+module.exports = router;
